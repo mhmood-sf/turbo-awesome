c99eeed9deb04bcb8bf3f7190c11a4dae6ae7fd0
quintik <28828855+quintik@users.noreply.github.com>
2022-08-21 02:17:52 +0800

    fix titlebars for new clients not being colored properly + clean up

--- cfg/gamma/ui/titlebar.lua
+++ cfg/gamma/ui/titlebar.lua
@@ -3,11 +3,16 @@ local wibox = require "wibox"
 local gears = require "gears"
 local beautiful = require "beautiful"
 
-local drag_normal_color = beautiful.titlebar_drag_button_normal or beautiful.colors.grey or "#999999"
-local drag_focus_color = beautiful.titlebar_drag_button_focus or beautiful.colors.accent or "#0000ff"
-local quit_color = beautiful.titlebar_close_button_normal or beautiful.colors.red or "#ff0000"
-local max_color = beautiful.titlebar_maximized_button_normal or beautiful.colors.green or "#00ff00"
-local min_color = beautiful.titlebar_minimize_button_normal or beautiful.colors.yellow or "#ffff00"
+local color = {
+    drag_focus = beautiful.titlebar_drag_button_focus or "#0000ff",
+    drag_normal = beautiful.titlebar_drag_button_normal or "#999999",
+    quit_focus = beautiful.titlebar_close_button_focus or "#ff0000",
+    quit_normal = beautiful.titlebar_close_button_normal or "#999999",
+    max_focus = beautiful.titlebar_maximized_button_focus or "#00ff00",
+    max_normal = beautiful.titlebar_maximized_button_normal or "#999999",
+    min_focus = beautiful.titlebar_minimize_button_focus or "#ffff00",
+    min_normal = beautiful.titlebar_minimize_button_normal or "#999999"
+}
 
 local tb_left_shape = function(cr, w, h)
     return gears.shape.partially_rounded_rect(cr, w, h, true, false, false, true, 10)
@@ -17,25 +22,45 @@ local tb_right_shape = function(cr, w, h)
     return gears.shape.partially_rounded_rect(cr, w, h, false, true, true, false, 10)
 end
 
-local button_surface = function(color)
+local button_surface = function(shape_color)
     return gears.surface.load_from_shape(
         25,
         25,
         gears.shape.circle,
-        color
+        shape_color
     )
+end
+
+local set_right_titlebar = function(c)
+    local right_titlebar = awful.titlebar(c, {
+        position = "right",
+        size = 25,
+        bg_focus = gears.color.transparent,
+        bg_normal = gears.color.transparent
+    })
+
+    right_titlebar:setup {
+        widget = wibox.container.background,
+        bg = beautiful.titlebar_bg_normal,
+        shape = tb_right_shape,
+        {
+            widget = wibox.widget.base.empty_widget
+        }
+    }
 end
 
 -- Signals to change drag button color on focus/unfocus.
 client.connect_signal("focus", function(c)
---    local tb = awful.titlebar(c, { position = "left" })
---    tb.widget.third.widget.image = button_surface(drag_focus_color)
+    -- When the first "focus" signal is emitted after the client
+    -- is created, the titlebars may not be initialized properly,
+    -- which would give us an error because `c` does not yet
+    -- have any `update_focus` property. Thus we need a nil-check
+    -- before calling it.
     if c.update_focus then c.update_focus(true) end
 end)
 
 client.connect_signal("unfocus", function(c)
---    local tb = awful.titlebar(c, { position = "left" })
---    tb.widget.third.widget.image = button_surface(drag_normal_color)
+    -- Same as above.
     if c.update_focus then c.update_focus(false) end
 end)
 
@@ -75,11 +100,32 @@ return function(c)
         end)
     )
 
-    local focus_widget = wibox.widget {
+    local focus_button_widget = wibox.widget {
         widget = wibox.widget.imagebox,
-        image = button_surface(drag_normal_color),
+        image = button_surface(color.drag_normal),
         resize = true,
         buttons = focus_button
+    }
+
+    local quit_button_widget = wibox.widget {
+        widget = wibox.widget.imagebox,
+        image = button_surface(color.quit_normal),
+        resize = true,
+        buttons = quit_button
+    }
+
+    local max_button_widget = wibox.widget {
+        widget = wibox.widget.imagebox,
+        image = button_surface(color.max_normal),
+        resize = true,
+        buttons = max_button
+    }
+
+    local min_button_widget = wibox.widget {
+        widget = wibox.widget.imagebox,
+        image = button_surface(color.min_normal),
+        resize = true,
+        buttons = min_button
     }
 
     local titlebar = awful.titlebar(c, {
@@ -103,36 +149,21 @@ return function(c)
                     bottom = 10,
                     left = 7,
                     right = 7,
-                    {
-                        widget = wibox.widget.imagebox,
-                        image = button_surface(quit_color),
-                        resize = true,
-                        buttons = quit_button
-                    }
+                    quit_button_widget
                 },
                 {
                     widget = wibox.container.margin,
                     bottom = 10,
                     left = 7,
                     right = 7,
-                    {
-                        widget = wibox.widget.imagebox,
-                        image = button_surface(max_color),
-                        resize = true,
-                        buttons = max_button
-                    }
+                    max_button_widget
                 },
                 {
                     widget = wibox.container.margin,
                     bottom = 10,
                     left = 7,
                     right = 7,
-                    {
-                        widget = wibox.widget.imagebox,
-                        image = button_surface(min_color),
-                        resize = true,
-                        buttons = min_button
-                    }
+                    min_button_widget
                 },
             },
             nil,
@@ -141,41 +172,39 @@ return function(c)
                 bottom = 10,
                 left = 7,
                 right = 7,
-                focus_widget--[[
-                {
-                    widget = wibox.widget.imagebox,
-                    image = button_surface(drag_normal_color),
-                    resize = true,
-                    buttons = focus_button
-                }]]
+                focus_button_widget
             }
         }
     }
 
-    -- For kitty windows, we add another titlebar to the left
-    -- to get proper rounding lol.
+    -- For kitty windows, we add another titlebar to
+    -- the left to get smoother rounding lol.
+    -- Otherwise set c.shape to round_rect
     if c.class == "kitty" or c.name == "bash" then
-        local right_titlebar = awful.titlebar(c, {
-            position = "right",
-            size = 25,
-            bg_focus = gears.color.transparent,
-            bg_normal = gears.color.transparent
-        })
-
-        right_titlebar:setup {
-            widget = wibox.container.background,
-            bg = beautiful.colors.black,
-            shape = tb_right_shape,
-            {
-                widget = wibox.widget.base.empty_widget
-            }
-        }
+        set_right_titlebar(c)
     else
-        -- Otherwise set c.shape to round the other side
         c.shape = tb_right_shape
     end
 
     c.update_focus = function(focused)
-        focus_widget.image = button_surface(focused and drag_focus_color or drag_normal_color)
+        if focused then
+            quit_button_widget.image = button_surface(color.quit_focus)
+            max_button_widget.image = button_surface(color.max_focus)
+            min_button_widget.image = button_surface(color.min_focus)
+            focus_button_widget.image = button_surface(color.drag_focus)
+        else
+            quit_button_widget.image = button_surface(color.quit_normal)
+            max_button_widget.image = button_surface(color.max_normal)
+            min_button_widget.image = button_surface(color.min_normal)
+            focus_button_widget.image = button_surface(color.drag_normal)
+        end
     end
+
+    -- It seems like the INITIAL "focus" signal is emitted before
+    -- the titlebars are initialized properly, which means when new
+    -- windows are created, their buttons aren't colored properly
+    -- even if the client is focused. Which is why after initializing
+    -- the titlebars, we run the update_focus function once to make
+    -- sure the buttons are colored properly nicely.
+    c.update_focus(true)
 end
